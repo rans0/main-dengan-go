ad68c4d200ef36f9af8dd5c8b02f9f8b7caffdd3
rans0 <kharansyah1@gmail.com>
2020-09-04 09:52:17 +0700

    converton fahrenheit to kelvin

--- temperatureConvertion.go
+++ temperatureConvertion.go
@@ -17,21 +17,45 @@ func CtoKelvin(t Celsius) Kelvin {
 	return Kelvin(t+273.15)
 }
 
-// function to convert fahrenheit to celcius
+// function to convert fahrenheit to celsius
 func FtoCelcius(t Fahrenheit) Celsius {
 	return Celsius((t-32)*5/9)
+}
+
+// Function to convert fahrenheit to kelvin
+func FtoKelvin(t Fahrenheit) Kelvin {
+	return Kelvin((t-32)*5/9+273.15)
+}
+
+// function to convert kelvin to fahrenheit
+func KtoFahrenheit(t Kelvin) Fahrenheit {
+	return Fahrenheit((t-273.15)*9/5+32)
 }
 
 func main() {
 
 	var Fahrenheit Fahrenheit = 100
 	var Celsius Celsius = 100
+	var Kelvin Kelvin = 100
 
-	tempFahr := CtoFahrenheit(Celsius)
-	tempKelvin := CtoKelvin(Celsius)
+	// set fahrenheit convertion from
+	//tempFahr := CtoFahrenheit(Celsius)
+	//fmt.Printf("%.2f C is equal to %.2f F\n", Celsius, tempFahr)
+	tempFahr := KtoFahrenheit(Kelvin)
+	fmt.Printf("%.2f K is equal to %.2f F\n", Kelvin, tempFahr)
+
+	// set celsius convertion from
 	tempCelsius := FtoCelcius(Fahrenheit)
-	fmt.Printf("%.2f C is equal to %.2f F\n", Celsius, tempFahr)
-	fmt.Printf("%.2f C is equal to %.2f K\n", Celsius, tempKelvin)
 	fmt.Printf("%.2f F is equal to %.2f C\n", Fahrenheit, tempCelsius)
+
+	// set kelvin convertion from
+	//tempKelvin := CtoKelvin(Celsius)
+	//fmt.Printf("%.2f C is equal to %.2f K\n", Celsius, tempKelvin)
+	tempKelvin := FtoKelvin(Fahrenheit)
+	fmt.Printf("%.2f F is equal to %.2f K\n", Fahrenheit, tempKelvin)
+
+
+
+
 
 }
