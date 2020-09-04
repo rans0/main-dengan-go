2a7c5aff73b41b8d4f27c448c02b1c9c864bd538
rans0 <kharansyah1@gmail.com>
2020-09-04 09:01:19 +0700

    fahrenheit to celsius

--- temperatureConvertion.go
+++ temperatureConvertion.go
@@ -7,16 +7,23 @@ type Celsius float32
 type Fahrenheit float32
 
 // function to convert celcius to fahrenheit
-func toFahrenheit(t Celsius) Fahrenheit {
-
+func CtoFahrenheit(t Celsius) Fahrenheit {
 	return Fahrenheit((t*9/5)+32)
+}
 
+// function to convert fahrenheit to celcius
+func FtoCelcius(t Fahrenheit) Celsius {
+	return Celsius((t-32)*5/9)
 }
 
 func main() {
 
+	var tempFahr Fahrenheit = 100
 	var tempCelsius Celsius = 100
-	tempFahr := toFahrenheit(tempCelsius)
-	fmt.Printf("%f C is equal to %f F", tempCelsius, tempFahr)
+
+	tempFahr = CtoFahrenheit(tempCelsius)
+	tempCelsius = FtoCelcius(tempFahr)
+	fmt.Printf("%f C is equal to %f F\n", tempCelsius, tempFahr)
+	fmt.Printf("%f F is equal to %f C\n", tempFahr, tempCelsius)
 
 }
