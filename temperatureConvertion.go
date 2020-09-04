29d62873ff28c4cd4b753586b94c9a8cc8cd3ceb
rans0 <kharansyah1@gmail.com>
2020-09-04 09:14:43 +0700

    celsius to kelvin

--- temperatureConvertion.go
+++ temperatureConvertion.go
@@ -5,10 +5,16 @@ import "fmt"
 // aliasing type
 type Celsius float32
 type Fahrenheit float32
+type Kelvin float32
 
 // function to convert celcius to fahrenheit
 func CtoFahrenheit(t Celsius) Fahrenheit {
 	return Fahrenheit((t*9/5)+32)
+}
+
+// function to convert celcius to kelvin
+func CtoKelvin(t Celsius) Kelvin {
+	return Kelvin(t+273.15)
 }
 
 // function to convert fahrenheit to celcius
@@ -18,12 +24,14 @@ func FtoCelcius(t Fahrenheit) Celsius {
 
 func main() {
 
-	var tempFahr Fahrenheit = 100
-	var tempCelsius Celsius = 100
+	var Fahrenheit Fahrenheit = 100
+	var Celsius Celsius = 100
 
-	tempFahr = CtoFahrenheit(tempCelsius)
-	tempCelsius = FtoCelcius(tempFahr)
-	fmt.Printf("%f C is equal to %f F\n", tempCelsius, tempFahr)
-	fmt.Printf("%f F is equal to %f C\n", tempFahr, tempCelsius)
+	tempFahr := CtoFahrenheit(Celsius)
+	tempKelvin := CtoKelvin(Celsius)
+	tempCelsius := FtoCelcius(Fahrenheit)
+	fmt.Printf("%.2f C is equal to %.2f F\n", Celsius, tempFahr)
+	fmt.Printf("%.2f C is equal to %.2f K\n", Celsius, tempKelvin)
+	fmt.Printf("%.2f F is equal to %.2f C\n", Fahrenheit, tempCelsius)
 
 }
