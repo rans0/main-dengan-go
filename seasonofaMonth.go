aa080766b44005cbbd4e2657743326785fe2a1b4
rans0 <kharansyah1@gmail.com>
2020-09-06 23:52:28 +0700

    challange switch case

---
+++ seasonofaMonth.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func Season(month int) string {
+
+	switch month {
+	case 1,2,12:
+		return "Winter"
+	case 3,4,5:
+		return "Spring"
+	case 6,7,8:
+		return "Summer"
+	case 9,10,11:
+		return "Autumn"
+	default:
+		return "Unknown season"
+	}
+	
+}
+
+func main() {
+	var month int = 10
+	fmt.Println(Season(month))
+}
