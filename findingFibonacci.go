166025233eca413d6e270bf39d0be444978b4914
rans0 <kharansyah1@gmail.com>
2020-09-11 00:54:46 +0700

    find fibonacci using slice

--- findingFibonacci.go
+++ findingFibonacci.go
@@ -3,7 +3,8 @@ import "fmt"
 
 var fib [10]int64
 
-func fibonacci() [10]int64 {
+func fibonacciWithArray() [10]int64 {
+
 	fib[0] = 1
 	fib[1] = 1
 
@@ -13,10 +14,28 @@ func fibonacci() [10]int64 {
 	return fib
 }
 
-func main() {
-	arr := fibonacci()
+func fibonacciWithSlice(term int) []int{
 
+	arr := make([]int, term)
+	arr[0], arr[1] = 1, 1
+
+	for i:=2; i < term; i ++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr
+}
+
+func main() {
+
+	arr := fibonacciWithArray()
 	for i:=0; i < 10; i ++ {
-		fmt.Printf("The %d-th Fibonacci number is : %d\n", i, arr[i])
+		fmt.Printf( "The %d-th Fibonacci number using ARRAY is : %d\n", i, arr[i])
+	}
+
+	fmt.Println("##############################")
+
+	slice := fibonacciWithSlice(10)
+	for i, fib := range slice {
+		fmt.Printf("The %d-th Fibonacci number using SLICE is : %d\n", i, fib)
 	}
 }
