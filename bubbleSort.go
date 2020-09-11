9d635c2d5b4135c30e59f7520b303a014de20fe9
rans0 <kharansyah1@gmail.com>
2020-09-12 01:58:26 +0700

    bubble sort descending

--- bubbleSort.go
+++ bubbleSort.go
@@ -13,6 +13,17 @@ func sortAsc(n []int) []int{
 	return n
 }
 
+func sortDesc(n []int) []int {
+	for pass:=1; pass < len(n); pass++ {
+		for i:=0; i < len(n) - pass; i++ {
+			if n[i] < n[i+1] {
+				n[i], n[i+1] = n[i+1], n[i]
+			}
+		}
+	}
+	return n
+}
+
 func main() {
 	number := []int{4,5,6,1,2,3,2,1}
 	fmt.Println("sorted ascending : ", sortAsc(number))
