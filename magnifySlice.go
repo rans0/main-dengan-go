c43e5f1b65018a6b8dbe939629acb5921464f327
rans0 <kharansyah1@gmail.com>
2020-09-11 22:58:54 +0700

    filling new slice

--- magnifySlice.go
+++ magnifySlice.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
-func enlarge(s []int, factor int) []int{
+func enlarge(s []int, factor int) []int {
 
 	newslice := make([]int, len(s)*factor)
 	copy(newslice, s)
 	return newslice
 }
+
+func enlargeAndFill(s []int, factor int) []int {
+	large := len(s)*factor
+	for i:=len(s); i < large; i++ {
+		s = append(s, i)
+	}
+	return s
+}
+
+
 
 func main() {
 	slice := []int{0,1,2}
@@ -16,4 +26,8 @@ func main() {
 	slice = enlarge(slice, 5)
 	fmt.Println("After enlarge is ", len(slice))
 	fmt.Println(slice)
+	slice2 := []int{0,1,2}
+	slice2 = enlargeAndFill(slice2, 5)
+	fmt.Println("After enlarge and fill is ", len(slice2))
+	fmt.Println(slice2)
 }
