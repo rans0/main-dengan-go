28630661e7fb05a1721ed15c97e4461fd8748346
rans0 <kharansyah1@gmail.com>
2020-09-19 21:39:57 +0700

    write wiki page

---
+++ writeWikiPage/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func load(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestingPage", Body: []byte("This is testing write to file")}
+	p1.save()
+	p2, _ := load("TestingPage")
+	fmt.Println(string(p2.Body))
+}
+
