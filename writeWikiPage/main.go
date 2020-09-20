1bc4e6b33d65da698dc1b694910c4506fc2cbcfa
rans0 <kharansyah1@gmail.com>
2020-09-20 20:29:42 +0700

    templating html

--- writeWikiPage/main.go
+++ writeWikiPage/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"html/template"
+	"regexp"
+	"log"
 )
 
 type Page struct {
@@ -10,13 +14,16 @@ type Page struct {
 	Body []byte
 }
 
+const  lenPath = len("/view/")
+
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func load(title string) (*Page, error) {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
+	fmt.Println("Title: ", filename)
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -24,10 +31,64 @@ func load(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !validPath.MatchString(r.URL.Path) {
+			fmt.Fprint(w, "Wrong URL!", r.URL.Path)
+			return
+		}
+		title := r.URL.Path[lenPath:]
+		if title == "" {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, title)
+	}
+}
+
 func main() {
-	p1 := &Page{Title: "TestingPage", Body: []byte("This is testing write to file")}
-	p1.save()
-	p2, _ := load("TestingPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
 }
 
