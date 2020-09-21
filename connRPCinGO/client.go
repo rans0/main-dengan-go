3c8f7ad2b4bd95433ec2aed2a7c31db48ffb699a
rans0 <kharansyah1@gmail.com>
2020-09-21 22:18:15 +0700

    implement rpc in go '

--- connRPCinGO/client.go
+++ connRPCinGO/client.go
@@ -1,1 +1,25 @@
-package connRPCinGO
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+	"learnGo/connRPCinGO/rpcobjects"
+)
+
+const serverAddress = "localhost"
+
+func main() {
+	client, err := rpc.Dial("tcp", serverAddress + ":3001")
+	if err != nil {
+		log.Fatal("Error dialing: ", err)
+	}
+	// Synchronous call
+	args := &rpcobjects.Args{3, 6}
+	var reply int
+	err = client.Call("Args.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("Args error:", err)
+	}
+	fmt.Printf("Args: %d * %d = %d", args.N, args.M, reply)
+}
--- connRPCinGO/server.go
+++ connRPCinGO/server.go
@@ -1,1 +1,22 @@
-package connRPCinGO
+package main
+
+import (
+	"net/http"
+	"log"
+	"net"
+	"net/rpc"
+	"time"
+	"learnGo/connRPCinGO/rpcobjects"
+)
+
+func main() {
+	calc := new(rpcobjects.Args)
+	rpc.Register(calc)
+	rpc.HandleHTTP()
+	listener, e := net.Listen("tcp", "0.0.0.0:3001")
+	if e != nil {
+		log.Fatal("Starting RPC=server -listen error: ", e)
+	}
+	go http.Serve(listener, nil)
+	time.Sleep(1000e9)
+}
